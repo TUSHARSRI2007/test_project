f0715d2ad266983028973e2a99e86a42619752ee
Tushar_Vinakoti <114989173+TUSHARSRI2007@users.noreply.github.com>
2024-05-20 23:08:33 +0530

    Update wsgi.py

--- zepxtools/wsgi.py
+++ zepxtools/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if os.environ.get('ENV') == 'production':
-    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zepxtools.settings')
 
